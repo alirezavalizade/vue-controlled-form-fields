99cda405b8f9bf55e00c9045b6d58acca9326b74
alireza <alirezavalizade0@gmail.com>
2019-11-27 22:22:53 +0100

    added less and sass loader for dev and examples 😱

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'vue-style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true
+            }
+          }
+        ]
+      }
+    ]
+  }
+};

5e0b51a1dfda588efa9fddd53004c7547883b2b3
alireza <alirezavalizade0@gmail.com>
2019-11-29 22:59:03 +0100

    added alis to webpack config 🤙

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
+const path = require('path');
+
 module.exports = {
+  chainWebpack: config => {
+    config.resolve.alias.set(
+      'vue-controlled-form-fields',
+      path.resolve(__dirname, 'src')
+    );
+  },
   css: {
     loaderOptions: {
       less: {

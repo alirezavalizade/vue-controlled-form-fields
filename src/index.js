e44a805b1bf6020c0a86e78e2b3158c7fda20cf6
alireza <alirezavalizade0@gmail.com>
2019-12-01 15:33:56 +0100

    export FieldArray component 🤫

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Form } from './components/Form.vue';
 export { default as Field } from './components/Field.vue';
+export { default as FieldArray } from './components/FieldArray.vue';
 export { default as FieldStateMixin } from './mixins/FieldStateMixin.js';

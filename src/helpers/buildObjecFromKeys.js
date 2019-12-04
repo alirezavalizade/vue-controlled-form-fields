a48e6b1204f0930078cf36042cd127c30507a3d0
alireza <alirezavalizade0@gmail.com>
2019-12-04 20:26:16 +0100

    fixed issue on getting fieldState 🐛

--- src/helpers/buildObjecFromKeys.js
+++ src/helpers/buildObjecFromKeys.js
@@ -1,8 +1,12 @@
 import set from 'lodash.set';
 
-const buildObjecFromKeys = (objWithKeys, value) => {
+const buildObjecFromKeys = (objWithKeys, getState, property) => {
   return Object.keys(objWithKeys).reduce((acc, name) => {
-    if (!Array.isArray(value)) {
+    const value = getState(name)[property];
+    if (
+      (Array.isArray(value) && value.some(v => typeof v === 'string')) ||
+      !Array.isArray(value)
+    ) {
       set(acc, name, value);
     }
     return acc;

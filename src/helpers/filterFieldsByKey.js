3fba05eb74fce817e6ac8fd924ea8e98691b0657
alireza <alirezavalizade0@gmail.com>
2019-12-04 20:27:30 +0100

    check type of array using Array.isArray - Used isUndefined method 🦍

--- src/helpers/filterFieldsByKey.js
+++ src/helpers/filterFieldsByKey.js
@@ -1,12 +1,17 @@
+import isUndefined from './isUndefined';
+
 const filterFieldsByKey = (
   fieldKeys,
   getFieldState,
   property,
   checkForValue
 ) => {
+  if (!Array.isArray(fieldKeys)) {
+    return {};
+  }
   return fieldKeys.reduce((acc, path) => {
     const value = getFieldState(path)[property];
-    if (checkForValue && typeof value !== 'undefined') {
+    if (checkForValue && !isUndefined(value)) {
       acc[path] = true;
     } else if (value) {
       acc[path] = true;

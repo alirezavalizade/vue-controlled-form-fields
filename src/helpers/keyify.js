9ba31b0bd00a0024a09137a410ceb151e56f6a46
alireza <alirezavalizade0@gmail.com>
2019-12-04 20:24:46 +0100

    fixed issue for nested arrays 🐛

--- src/helpers/keyify.js
+++ src/helpers/keyify.js
@@ -1,12 +1,19 @@
 const keyify = (obj, prefix = '', prevPrefix = '') => {
+  if (typeof obj !== 'object') {
+    return [];
+  }
   return Object.keys(obj).reduce((res, key) => {
     if (Array.isArray(obj[key])) {
       return [...res, ...keyify(obj[key], `${prefix}${key}[`, `]`)];
     } else if (typeof obj[key] === 'object') {
-      if (!Object.keys(obj[key]).length && typeof Number(key) === 'number') {
-        return [...res, `${prefix}${key}]`];
-      } else if (!obj[key].hasOwnProperty('touched')) {
-        return [...res, ...keyify(obj[key], `${prefix}${key}.`, prevPrefix)];
+      if (!prevPrefix) {
+        if (!Object.keys(obj[key]).length && typeof Number(key) === 'number') {
+          return [...res, `${prefix}${key}]`];
+        } else if (!obj[key].hasOwnProperty('touched')) {
+          return [...res, ...keyify(obj[key], `${prefix}${key}.`, prevPrefix)];
+        }
+      } else {
+        return [...res, ...keyify(obj[key], `${prefix}${key}].`)];
       }
     }
     return [...res, `${prefix}${key}${prevPrefix}`];

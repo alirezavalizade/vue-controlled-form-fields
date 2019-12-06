ecc2c555f179c3a9c4d3216b585ee7f83f812747
alireza <alirezavalizade0@gmail.com>
2019-12-06 14:50:37 +0100

    made subscribe function subscription based 🎉 - used dynamic watch trick

--- src/components/mixins/SubscribeMixin.js
+++ src/components/mixins/SubscribeMixin.js
@@ -1,29 +1,73 @@
+import isEqual from 'lodash.isequal';
+import { formSubscriptionItems } from '../../helpers/subscriptionItems';
+import getDefaultSubscriptionItems from '../../helpers/getDefaultSubscriptionItems';
+
 export default {
-  data() {
-    return {
-      subscribers: {}
-    };
+  data: () => ({
+    formListeners: []
+  }),
+  created() {
+    this.subscribeFormListeners();
   },
-  watch: {
-    values() {
-      this.notifySubscribers();
-    }
+  destroyed() {
+    this.unsubscribeFormListeners();
   },
   methods: {
-    handleSubscribe(subscriber) {
-      const subscriberID = Math.random();
-      this.$set(this.subscribers, subscriberID, subscriber);
+    handleSubscribe(subscriber, subscription = getDefaultSubscriptionItems()) {
+      let unwatch;
+      if (subscription.default) {
+        unwatch = this.$watch(
+          function() {
+            return this.formStateReport;
+          },
+          function(state) {
+            subscriber(state);
+          }
+        );
+      } else {
+        const keys = Object.keys(subscription);
+        unwatch = this.$watch(
+          function() {
+            return keys.reduce((acc, key) => {
+              acc[key] = this[key];
+
+              return acc;
+            }, {});
+          },
+          function(state, oldState) {
+            if (!isEqual(state, oldState)) {
+              subscriber(state);
+            }
+          }
+        );
+      }
 
       return () => {
-        this.$delete(this.subscribers, subscriberID);
+        if (subscription.default) {
+          return unwatch();
+        }
       };
     },
-    notifySubscribers() {
-      // TODO: Add custom listeners
-      const subscriberIDs = Object.keys(this.subscribers);
-      for (var i = 0; i < subscriberIDs.length; i++) {
-        this.subscribers[subscriberIDs](this.formStateReport);
+    subscribeFormListeners() {
+      const listenersKeys = Object.keys(this.$listeners);
+      const validListenersKeys = listenersKeys.filter(key =>
+        formSubscriptionItems.includes(key)
+      );
+      for (let i = 0; i < validListenersKeys.length; i++) {
+        this.formListeners[i] = this.form.subscribe(
+          this.$listeners[validListenersKeys[i]],
+          {
+            [validListenersKeys[i]]: true
+          }
+        );
       }
+      // Avoid using update incase @change hasn't passed to form
+      if (listenersKeys.includes('change')) {
+        this.form.subscribe(this.$listeners.change);
+      }
+    },
+    unsubscribeFormListeners() {
+      this.formListeners.forEach(un => un());
     }
   }
 };

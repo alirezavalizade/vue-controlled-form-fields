5f857a68bf03d7a7af00d95a7e9e2716551ad3db
alireza <alirezavalizade0@gmail.com>
2019-12-06 13:52:21 +0100

    added subsciption items 🐍

---
+++ src/helpers/subscriptionItems.js
@@ -0,0 +1,29 @@
+export const formSubscriptionItems = [
+  'dirty',
+  'valid',
+  'invalid',
+  'visited',
+  'modified',
+  'pristine',
+  'touched',
+  'active',
+  'submitting',
+  'values',
+  'errors',
+  'dirtyFields',
+  'modifiedFields',
+  'visitedFields',
+  'initialValues'
+];
+
+export const fieldSubscriptionItems = [
+  'active',
+  'dirty',
+  'error',
+  'invalid',
+  'modified',
+  'pristine',
+  'touched',
+  'valid',
+  'visited'
+];
